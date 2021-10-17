1a069a20197649749d5129651c7b7894e9b90b26
Paloma Queiroz <palomacqueiroz@hotmail.com>
2021-10-17 11:11:08 -0300

    components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,10 @@ import { Icon } from '@iconify/react';
 import { removeStorageKey } from '../../services/storage';
 import vixiCangaço from '../../Assets/burger.png';
 import vixiKitchen from '../../Assets/logodesktop.png';
-// import { getRouteHeaderMenu } from '../../routes/redirections';
 import './style.scss'
 
 const Header = () => {
     const history = useHistory();
-
-    // const handleRoute  = (path) => {
-    //     const route = getRouteHeaderMenu(path)
-    //     history.push(route)
-    // }
 
     const handleLogout = () => {
         removeStorageKey();
@@ -22,7 +16,6 @@ const Header = () => {
     return (
         <nav className="header-navbar-desktop">
             <img className="logo" src={vixiCangaço} alt='icon-vixi' />
-            {/* <h2 className="header-title">VIXI!</h2> */}
             <Link to='/menu' className="header-button">CARDÁPIO</Link>
             <Link to='/orders-status' className="header-button">STATUS DOS PEDIDOS</Link>
             <button className="header-button" onClick={handleLogout}><Icon icon="carbon:logout" color="black" /></button>
@@ -40,7 +33,6 @@ const HeaderKitchen = () => {
     return (
         <nav className="header-navbar-desktop">
             <img className="logoKitcken" src={vixiKitchen} alt='icon-vixi' />
-            {/* <h2 className="header-title">VIXI!</h2> */}
             <h2 className="header-title-kitchen">Cozinha • Pedidos</h2>
             <button className="header-button" onClick={handleLogout}><Icon icon="carbon:logout" color="black" /></button>
         </nav>
